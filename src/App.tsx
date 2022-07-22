706d9c56ad89a24b13817e1918fdaf2947407fcc
oseasmoreto <oseas_moreto@hotmail.com>
2022-07-22 12:25:36 -0300

    Adicionando tema com styled components

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import { useState } from 'react'
+import { ThemeProvider } from 'styled-components'
 import { Button } from './components/Button'
+import { defaultTheme } from './styles/theme/default'
 
 export function App() {
 
   return (
-    <div className="App">
+    <ThemeProvider theme={defaultTheme}>
       <Button />
       <Button variant="secondary" />
-    </div>
+    </ThemeProvider>
   )
 }
