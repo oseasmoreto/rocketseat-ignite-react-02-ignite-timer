7729f29c69d631a026cc1c6647f91c59d4633d43
oseasmoreto <oseas_moreto@hotmail.com>
2022-07-22 12:08:58 -0300

    Aprendendo sobre styled components

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import { useState } from 'react'
+import { Button } from './components/Button'
 
 export function App() {
 
   return (
     <div className="App">
-      <h1>ola</h1>
+      <Button />
+      <Button variant="secondary" />
     </div>
   )
 }
---
+++ src/components/Button/index.styles.ts
@@ -0,0 +1,25 @@
+import styled, {css} from 'styled-components';
+
+export type ButtonVariant = 'primary' | 'secondary' | 'danger' | 'success';
+
+interface ButtonContainerProps {
+  variant: ButtonVariant;
+}
+
+const buttonVariants = {
+  primary: 'pink',
+  secondary: 'purple',
+  danger: 'red',
+  success: 'green'
+}
+
+export const ButtonContainer = styled.button<ButtonContainerProps>`
+  width: 100px;
+  height: 40px;
+
+  ${props => {
+    return css`
+      background-color: ${buttonVariants[props.variant]};
+    `
+  }}
+`
